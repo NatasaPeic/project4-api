dabfdc0624549414cedcea9a2bd27d50cbef81ad
NatasaPeic <peic.natasa@gmail.com>
2016-09-18 10:35:18 -0400

    Add inverse of:memories

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   include Authentication
   has_many :examples
+  has_many :memories, inverse_of: :user, dependent: :destroy,
+           foreign_key: 'user_id'
 end
