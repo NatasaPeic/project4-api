062d99e927a739a228ccf23c20d1757cd587da4e
NatasaPeic <peic.natasa@gmail.com>
2016-09-27 09:16:44 -0400

    Add destroy action for admin and user
    - cleaned up the code from trying to get admin work

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,31 +1,47 @@
-class MemoriesController < ApplicationController
-  before_action :set_memory, only: [:show, :update, :destroy]
+class MemoriesController < OpenReadController
+  before_action :set_memory, only: [:update, :destroy]
 
 
-  # if params[:user_palettes] == "true"
-  #    @palettes = current_user.palettes
-  #  else
-  #    @palettes = Palette.all
-  #  end
+  # def index
+  #   @memories = Memory.all
+  #   # @memories = current_user.memories
+  #
+  #   render json: @memories
+  # end
 
 
-  # GET /memories
-  # GET /memories.json
+
   def index
-    @memories = Memory.all
-    # @memories = current_user.memories
-
+    if params[:all] == 'true'
+      @memories = Memory.all
+    else
+      @memories = current_user.memories
+    end
     render json: @memories
   end
+
+
+
+
+
+
+
 
   # GET /memories/1
   # GET /memories/1.json
   def show
-    render json: @memory
+      render json: Memory.find(params[:id])
+    # render json: @memory
   end
+
+  def new
+    @memory = current_user.memories.build(memory_params)
+  end
+
 
   # POST /memories
   # POST /memories.json
+  # missed memory_params for create, showed null on localhost:3000
   def create
     @memory = current_user.memories.build(memory_params)
 
@@ -36,11 +52,11 @@ class MemoriesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /memories/1
-  # PATCH/PUT /memories/1.json
+
+
   def update
     # @memory = Memory.find(params[:id])
-    @plant = current_user.memories.find(params[:id])
+    # @memory = current_user.memories.find(params[:id])
 
     if @memory.update(memory_params)
       head :no_content
@@ -49,12 +65,15 @@ class MemoriesController < ApplicationController
     end
   end
 
+
   # DELETE /memories/1
   # DELETE /memories/1.json
   def destroy
+
     @memory.destroy
 
     head :no_content
+
   end
 
   # private
@@ -68,5 +87,5 @@ class MemoriesController < ApplicationController
       params.require(:memory).permit(:name, :description, :cohort)
     end
 
-    private :set_memory, :memory_params, :current_user
+     private :memory_params, :set_memory
 end
