d696a9c83e5f81e68f1227b25e9f879cc6f2c2da
NatasaPeic <peic.natasa@gmail.com>
2016-09-27 09:21:19 -0400

    Test routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :memories, except: [:new, :edit]
+  # resources :memories, except: [:new, :edit]
+  resources :memories, only: [:index, :show, :create, :update, :destroy]
   resources :examples, except: [:new, :edit]
+
+
+
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
