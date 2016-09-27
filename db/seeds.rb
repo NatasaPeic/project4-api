217db8d57484b430d2845b40fc741bf87288c766
NatasaPeic <peic.natasa@gmail.com>
2016-09-27 09:22:50 -0400

    Add seed data (admin)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Admin
+
+User.create!(email: 'natasapeic@gmail.com', password: '123', admin: true)
