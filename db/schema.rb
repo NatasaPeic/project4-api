dc51c643cd98bdea42176ac443832dc2c1237a71
NatasaPeic <peic.natasa@gmail.com>
2016-09-29 12:43:09 -0400

    Alter database
    - add date

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923194123) do
+ActiveRecord::Schema.define(version: 20160928230131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,9 +32,20 @@ ActiveRecord::Schema.define(version: 20160923194123) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "date"
   end
 
   add_index "memories", ["user_id"], name: "index_memories_on_user_id", using: :btree
+
+  create_table "uploads", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
